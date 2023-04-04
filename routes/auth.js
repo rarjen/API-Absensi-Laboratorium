a73b8d6453d8342336ceb919b56ba352bb1f2081
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-04-04 15:26:12 +0700

    make router for auth

---
+++ routes/auth.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const authorize = require("../middlewares/authorize");
+const auth = require("../controllers/auth");
+
+router.post("/register", auth.registerAccount);
+router.post("/login", auth.loginAccount);
+
+module.exports = router;
