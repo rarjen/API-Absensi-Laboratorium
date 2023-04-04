b710cdc50dd51729246f48d91df1a883dcefb7a9
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-04-04 18:05:37 +0700

    create relations

--- models/absensi.js
+++ models/absensi.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Absensi extends Model {
     /**
@@ -10,20 +8,26 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Absensi.belongsTo(models.Karyawan, {
+        foreignKey: "id_karyawan",
+        as: "karyawan",
+      });
     }
   }
-  Absensi.init({
-    id_karyawan: DataTypes.INTEGER,
-    jam_masuk: DataTypes.STRING,
-    jam_pulang: DataTypes.STRING,
-    tipe: DataTypes.STRING,
-    status: DataTypes.STRING,
-    image_url: DataTypes.STRING,
-    tanggal: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Absensi',
-  });
+  Absensi.init(
+    {
+      id_karyawan: DataTypes.INTEGER,
+      jam_masuk: DataTypes.STRING,
+      jam_pulang: DataTypes.STRING,
+      tipe: DataTypes.STRING,
+      status: DataTypes.STRING,
+      image_url: DataTypes.STRING,
+      tanggal: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Absensi",
+    }
+  );
   return Absensi;
 };
