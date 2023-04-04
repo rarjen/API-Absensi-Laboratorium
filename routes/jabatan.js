eb781cdf7a35fa376e4106180c683219f51eaba0
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-04-04 17:08:32 +0700

    use middleware authorization

--- routes/jabatan.js
+++ routes/jabatan.js
@@ -4,9 +4,9 @@ const authorize = require("../middlewares/authorize");
 const jabatan = require("../controllers/jabatan");
 
 router.get("/", authorize(role.admin), jabatan.index);
-router.get("/:id_jabatan", jabatan.show);
-router.post("/create", jabatan.create);
-router.put("/edit/:id_jabatan", jabatan.update);
-router.delete("/edit/:id_jabatan", jabatan.destroy);
+router.get("/:id_jabatan", authorize(role.admin), jabatan.show);
+router.post("/create", authorize(role.admin), jabatan.create);
+router.put("/edit/:id_jabatan", authorize(role.admin), jabatan.update);
+router.delete("/edit/:id_jabatan", authorize(role.admin), jabatan.destroy);
 
 module.exports = router;
--- routes/karyawan.js
+++ routes/karyawan.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
-// const authorize = require("../middlewares/authorize");
+const authorize = require("../middlewares/authorize");
+const role = require("../utils/enum");
 const karyawan = require("../controllers/karyawan");
 
-router.get("/", karyawan.index);
-router.get("/:id_karyawan", karyawan.show);
-router.post("/create", karyawan.create);
-router.put("/edit/:id_karyawan", karyawan.update);
-router.delete("/delete/:id_karyawan", karyawan.destroy);
+router.get("/", authorize(role.admin), karyawan.index);
+router.get("/:id_karyawan", authorize(role.admin), karyawan.show);
+router.post("/create", authorize(role.admin), karyawan.create);
+router.put("/edit/:id_karyawan", authorize(role.admin), karyawan.update);
+router.delete("/delete/:id_karyawan", authorize(role.admin), karyawan.destroy);
 
 module.exports = router;
