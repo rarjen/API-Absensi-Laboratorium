7da13c482a71dadbf4cc863d63131fdcf9c0a16e
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-04-04 15:26:42 +0700

    modify authorization

--- routes/jabatan.js
+++ routes/jabatan.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-// const authorize = require("../middlewares/authorize");
+const role = require("../utils/enum");
+const authorize = require("../middlewares/authorize");
 const jabatan = require("../controllers/jabatan");
 
-router.get("/", jabatan.index);
+router.get("/", authorize(role.admin), jabatan.index);
 router.get("/:id_jabatan", jabatan.show);
 router.post("/create", jabatan.create);
 router.put("/edit/:id_jabatan", jabatan.update);
