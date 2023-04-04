6615465f10311bbab1b175889cc3344040857fa8
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-04-04 13:35:49 +0700

    make route for get jabatan by id

--- routes/jabatan.js
+++ routes/jabatan.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const jabatan = require("../controllers/jabatan");
 
 router.get("/", jabatan.index);
+router.get("/:id_jabatan", jabatan.show);
 router.post("/create", jabatan.create);
 router.put("/edit/:id_jabatan", jabatan.update);
 router.delete("/edit/:id_jabatan", jabatan.destroy);
