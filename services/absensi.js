a78c91f94ced7dabc512fdad9e7c1456ce8f709a
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-04-05 21:37:48 +0700

    make logic for absensi

--- services/absensi.js
+++ services/absensi.js
@@ -1,17 +1,65 @@
-// const { Absensi, Karyawan } = require("../models");
+const { Absensi, Karyawan } = require("../models");
+const { BadRequestError } = require("../errors");
 
-// const postAbsen = async (req) => {
-//   //   const { id_karyawan, timeDate, tipe, status, image_url } = req.body;
+const postAbsen = async (req) => {
+  let {
+    id_karyawan,
+    timeDate = new Date(),
+    tipe,
+    status,
+    image_url = "-",
+  } = req.body;
 
-//   const getKaryawan = await Karyawan.findOne({ where: { id: id_karyawan } });
+  const shiftPagi = "07:00";
+  const shiftSore = "14.00";
 
-//   return getKaryawan;
-// };
+  const getKaryawan = await Karyawan.findOne({ where: { id: id_karyawan } });
 
-// module.exports = { postAbsen };
+  if (!getKaryawan) {
+    throw new BadRequestError(`Tidak ada karyawan dengan id: ${id_karyawan}`);
+  }
 
-const date = new Date();
-const convert = date.toLocaleTimeString();
-// const time = convert.getTime();
+  const shiftKaryawan = getKaryawan.shift;
+  console.log(shiftKaryawan);
 
-console.log(convert);
+  if (tipe === "Pulang") {
+    const result = await Absensi.update(
+      {
+        jam_pulang: timeDate.toLocaleTimeString(),
+        tipe,
+      },
+      { where: { id_karyawan, tanggal: timeDate.toLocaleDateString() } }
+    );
+
+    if (result <= 0) {
+      throw new BadRequestError("Anda belum absen datang hari ini!");
+    }
+
+    return result;
+  }
+
+  if (shiftKaryawan === "Pagi" && timeDate.toLocaleTimeString() <= shiftPagi) {
+    status = "Tepat";
+  } else if (
+    shiftKaryawan === "Sore" &&
+    timeDate.toLocaleTimeString() <= shiftSore
+  ) {
+    status = "Tepat";
+  } else {
+    status = "Telat";
+  }
+
+  const result = await Absensi.create({
+    id_karyawan,
+    jam_masuk: timeDate.toLocaleTimeString(),
+    jam_pulang: "-",
+    tipe,
+    status,
+    image_url,
+    tanggal: timeDate.toLocaleDateString(),
+  });
+
+  return result;
+};
+
+module.exports = { postAbsen };
