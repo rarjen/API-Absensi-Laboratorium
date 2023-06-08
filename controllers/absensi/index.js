6655d8b02a79a5fab298faa58fbcbb29b20f4044
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-08 16:48:46 +0700

    modify controller absensi

--- controllers/absensi/index.js
+++ controllers/absensi/index.js
@@ -1,4 +1,9 @@
-const { postAbsen, getAbsensi } = require("../../services/absensi");
+const {
+  postAbsen,
+  getAbsensi,
+  todayAbsen,
+  postAbsenPulang,
+} = require("../../services/absensi");
 const { StatusCodes } = require("http-status-codes");
 
 const absen = async (req, res, next) => {
@@ -6,6 +11,19 @@ const absen = async (req, res, next) => {
     const result = await postAbsen(req);
 
     return res.status(StatusCodes.CREATED).json({
+      status: true,
+      message: "Data Recorded",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+const absenPulang = async (req, res, next) => {
+  try {
+    const result = await postAbsenPulang(req);
+
+    return res.status(StatusCodes.OK).json({
       status: true,
       message: "Data Recorded",
       data: result,
@@ -29,4 +47,18 @@ const show = async (req, res, next) => {
   }
 };
 
-module.exports = { absen, show };
+const showNow = async (req, res, next) => {
+  try {
+    const result = await todayAbsen(req);
+
+    return res.status(StatusCodes.OK).json({
+      status: true,
+      message: "Success get absen",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { absen, show, showNow, absenPulang };
