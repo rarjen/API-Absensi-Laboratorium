1b50e1983bd2fa24b37b408a75172131daaf541b
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-08 15:52:59 +0700

    make routes for shift

---
+++ routes/shift.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const authorize = require("../middlewares/authorize");
+const { ROLES } = require("../utils/enum");
+const shift = require("../controllers/shift");
+
+router.get("/", authorize(), shift.indexShift);
+router.get("/:shift_id", authorize(), shift.showShift);
+router.post("/create", authorize(ROLES.ADMIN), shift.createShift);
+router.put("/edit/:karyawan_id", authorize(ROLES.ADMIN), shift.editShift);
+
+module.exports = router;
