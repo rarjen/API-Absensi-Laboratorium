87b9d44f8ca8424f89752c9b4c914cb17255d4e2
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-07-03 20:07:56 +0700

    modify absensi routes

--- routes/absensi.js
+++ routes/absensi.js
@@ -7,4 +7,5 @@ router.post("/postAbsen", absensi.absen);
 router.put("/postAbsenPulang/:absen_id", absensi.absenPulang);
 router.get("/getTodayAbsen", absensi.showNow);
 router.get("/getAbsen", authorize(ROLES.ADMIN), absensi.show);
+router.get("/showAbsen/:absen_id", absensi.showAbsen);
 module.exports = router;
