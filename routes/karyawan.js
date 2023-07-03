66c4dbe0eaec63399240ef7fdd47290f0c12a17e
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-07-03 20:08:07 +0700

    modify karyawan routes

--- routes/karyawan.js
+++ routes/karyawan.js
@@ -3,10 +3,10 @@ const authorize = require("../middlewares/authorize");
 const { ROLES } = require("../utils/enum");
 const karyawan = require("../controllers/karyawan");
 
-router.get("/", authorize(), karyawan.index);
+router.get("/", karyawan.index);
 router.get("/:karyawan_id", authorize(), karyawan.show);
 router.post("/create", authorize(ROLES.ADMIN), karyawan.create);
 router.put("/edit/:karyawan_id", authorize(ROLES.ADMIN), karyawan.update);
-router.delete("/delete/:karyawan_id", authorize(ROLES.ADMIN), karyawan.destroy);
+router.delete("/:karyawan_id", authorize(ROLES.ADMIN), karyawan.destroy);
 
 module.exports = router;
