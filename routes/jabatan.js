ee7517b88527f1903b6824a9e67367bd2a57f373
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-07-25 08:42:07 +0700

    modify jabatan routes

--- routes/jabatan.js
+++ routes/jabatan.js
@@ -4,7 +4,7 @@ const authorize = require("../middlewares/authorize");
 const jabatan = require("../controllers/jabatan");
 
 router.get("/", authorize(role.admin), jabatan.index);
-router.get("/:id_jabatan", authorize(role.admin), jabatan.show);
+router.get("/:idJabatan", authorize(role.admin), jabatan.show);
 router.post("/create", authorize(role.admin), jabatan.create);
 router.put("/edit/:id_jabatan", authorize(role.admin), jabatan.update);
 router.delete("/delete/:id_jabatan", authorize(role.admin), jabatan.destroy);
