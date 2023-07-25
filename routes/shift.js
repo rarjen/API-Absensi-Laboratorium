62a2c8d230615057066ceb589d586ed362bb8ef0
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-07-25 08:42:15 +0700

    modify shift routes

--- routes/shift.js
+++ routes/shift.js
@@ -6,6 +6,6 @@ const shift = require("../controllers/shift");
 router.get("/", authorize(), shift.indexShift);
 router.get("/:shift_id", authorize(), shift.showShift);
 router.post("/create", authorize(ROLES.ADMIN), shift.createShift);
-router.put("/edit/:karyawan_id", authorize(ROLES.ADMIN), shift.editShift);
+router.put("/edit/:shift_id", authorize(ROLES.ADMIN), shift.editShift);
 
 module.exports = router;
